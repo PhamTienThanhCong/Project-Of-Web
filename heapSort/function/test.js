bf33a704a30d4c8e11f27deb479d7df0d798d7c0
congphamtienthanh <cong.pttc@gmail.com>
2021-11-26 21:54:08 +0700

    heapsort buoi 1

---
+++ heapSort/function/test.js
@@ -0,0 +1,18 @@
+//   Create all tree
+
+// tạo các mũi tên
+
+//   const printTreeArrows = [];
+
+//   for (var i=1; i<pointTrees.length; i++){
+//     var printTreeArrow = '<connection id='+ pointTrees[i].idArrowTree +' from="#'+pointTrees[Math.round(i/2-1)].idTree+'" to="#'+ pointTrees[i].idTree +'" color="#aaaaaa" fromX="0.5" fromY="0.5" tail onlyVisible></connection>' 
+//     printTreeArrows.push(printTreeArrow);
+//   }
+
+//   for (var i=1; i<pointTrees.length; i++){
+//       var param = document.createElement("div");
+//       param.innerText = printTreeArrows[i];
+//       document.body.appendChild(param);   
+//     }
+
+
