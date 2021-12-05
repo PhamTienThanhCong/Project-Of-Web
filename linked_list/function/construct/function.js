36ececf9ae60cfc65e13e3a3de7ef2b7ed4d01df
congphamtienthanh <cong.pttc@gmail.com>
2021-12-05 09:09:19 +0700

    cơ bản hoàn thành các node

--- linked_list/function/construct/function.js
+++ linked_list/function/construct/function.js
@@ -17,63 +17,14 @@ function convetArrayToString(a){
     return ArrayString;
 }
 
-function insertNodeShow(index, value) {
-    insertNode(index,value)
-    // NodeSort()
-
-    var node = document.createElement("div");
-    node.id = nodeId[nodeValue.length - 1]
-    node.innerHTML = nodeValue[nodeValue.length - 1]
-    // node.style.left = pointX[nodeValue[nodeShow.length - 1].point]+"px"
-    node.style.left = pointX[0]+"px"
-    node.style.top = "50px"
-    node.className = "chill-show"
-    document.getElementById("main-show-to-desk").appendChild(node)  
-
-    var show = []
-    var number = []
-    for (var i = 0; i <nodeValue.length; i++){
-        if (nodePoint[i] < index){
-            show.push(nodeId[i])
-            number.push(nodePoint[i])
-        }
+function resetNode() {
+    for (var j = 0; j < nodeId.length; j++) {
+        document.getElementById(nodeId[j]).style.border = "1px solid black"
+        document.getElementById(nodeId[j]).style.left = pointX[nodePoint[j]]+"px"
     }
-    NodeSort(show,number)
-    
-    var i=0;
-    const run = setInterval(function() {
-        if (i<show.length){
-            document.getElementById(show[i]).style.border = "2px solid red"
-        }else{
-            for (var j = 0; j <nodeValue.length; j++){
-                if (nodePoint[j] !== index){
-                    document.getElementById(nodeId[j]).style.left = pointX[nodePoint[j]]+"px"
-                }               
-            }
-            i = nodeValue.length;
-        }
-        i++;
-        if(i > nodeValue.length){       
-            var t = 0;
-            if (index === 0){
-                t=1;
-            }
-            const run2 = setInterval(function(){
-                if (t === 0){
-                    document.getElementById(nodeId[nodeValue.length - 1]).style.left = pointX[nodePoint[nodeValue.length - 1]]+"px"
-                }else if(t === 1){
-                    document.getElementById(nodeId[nodeValue.length - 1]).style.top = "150px"; 
-                }else if (t === 2){
-                    clearInterval(run2);
-                    resetNode()
-                    console.log("stop Insert")
-                }
-                t++;
-            },1000)
-            clearInterval(run);
-        }
-    },1000)
 }
+
+
 function NodeSort(show,number) {
     let n = number.length;
         for (let i = 0; i < n-1; i++) {
--- linked_list/function/construct/graphic.js
+++ linked_list/function/construct/graphic.js
@@ -5,7 +5,6 @@
 const widthScreen = 1000;
 var nodes = [];
 
-
 var pointX = [];
 var firstPoint = widthScreen / (10 + 1) - 25
 pointX.push(firstPoint)
@@ -22,6 +21,17 @@ var nodePoint = [0];
 var nodeId = ['node-NULL'];
 
 
+function showNode() {
+    var node = document.createElement("div");
+    node.id = nodeId[0]
+    node.innerHTML = nodeValue[0]
+    node.style.left = pointX[nodePoint[0]] + "px"
+    node.className = "chill-show"
+    document.getElementById("main-show-to-desk").appendChild(node)
+}
+showNode()
+insertNodeShow(0, 1)
+
 function insertNode(index, value) {
     ll.insertAt(index, value);
     for (var i = 0; i < nodeValue.length; i++) {
@@ -31,42 +41,38 @@ function insertNode(index, value) {
     }
     nodeValue.push(value)
     nodePoint.push(index)
-    nodeId.push("nodeIdRand" + index + "" + Math.floor(Math.random() * 10) + "")
+    nodeId.push("nodeIdRand" + index + "" + Math.floor(Math.random() * 1000) + "")
 }
 
-insertNodeShow(0, 1)
-
-
-
-function deleteNode(index) {
+function findNodeShow(index) {
     ll.removeFrom(index);
-
+    if (document.getElementById("move-node")){
+        document.getElementById("move-node").remove()
+    }
 
     var show = []
     var number = []
     for (var i = 0; i < nodeValue.length; i++) {
         if (nodePoint[i] <= index) {
-            console.log("show")
             show.push(nodeId[i])
             number.push(nodePoint[i])
         }
     }
     NodeSort(show, number)
-    console.log(show, number)
 
     var i = 0;
-    var a
     const run = setInterval(function () {
         if (i < show.length) {
-            document.getElementById(show[i]).style.border = "2px solid red"
+            document.getElementById(show[i]).style.border = "2px solid #66ff33"
         } else if(i === show.length){
-            for (var j = 0; j < nodeValue.length; j++) {
-                if (nodePoint[j] > index) {
-                    nodePoint[j] -= 1
-                }
-            }
-            document.getElementById(show[i-1]).style.backgroundColor = "red"
-            document.getElementById(show[i-1]).id = "move-node"
+            var newnode = document.createElement("div");
+            newnode.className = "chill-show"
+            newnode.style.left = document.getElementById(show[i-1]).style.left
+            newnode.style.top = document.getElementById(show[i-1]).style.top
+            newnode.id = "move-node"
+            newnode.style.backgroundColor = "#33cc33"
+            newnode.innerText = document.getElementById(show[i-1]).innerText
+            document.getElementById("main-show-to-desk").appendChild(newnode)
         }
         if(i > show.length){
             var t = 0;
@@ -76,9 +82,6 @@ function deleteNode(index) {
                 }else if(t === 1){
                     document.getElementById("move-node").style.left = "60px"; 
                 }else if (t === 2){
-                    nodeValue.splice(index, 1);
-                    nodePoint.splice(index, 1);
-                    nodeId.splice(index, 1);
                     clearInterval(run2);
                     resetNode()
                     console.log("stop Insert")
@@ -89,24 +92,120 @@ function deleteNode(index) {
         }
         i++;
     }, 1000)
-
-
-
 }
+function insertNodeShow(index, value) {
+    insertNode(index,value)
+    // NodeSort()
 
-function showNode() {
     var node = document.createElement("div");
-    node.id = nodeId[0]
-    node.innerHTML = nodeValue[0]
-    node.style.left = pointX[nodePoint[0]] + "px"
-    node.className = "chill-show"
-    document.getElementById("main-show-to-desk").appendChild(node)
-}
-showNode()
+    node.id = nodeId[nodeValue.length - 1]
+    node.innerHTML = nodeValue[nodeValue.length - 1]
 
-function resetNode() {
-    for (var j = 0; j < nodeId.length; j++) {
-        document.getElementById(nodeId[j]).style.border = "none"
-        document.getElementById(nodeId[j]).style.left = pointX[nodePoint[j]]+"px"
+    node.style.left = pointX[0]+"px"
+    node.style.top = "50px"
+    node.className = "chill-show"
+    document.getElementById("main-show-to-desk").appendChild(node)  
+
+    var show = []
+    var number = []
+    for (var i = 0; i <nodeValue.length; i++){
+        if (nodePoint[i] < index){
+            show.push(nodeId[i])
+            number.push(nodePoint[i])
+        }
     }
+    NodeSort(show,number)
+    
+    var i=0;
+    const run = setInterval(function() {
+        if (i<show.length){
+            document.getElementById(show[i]).style.border = "2px solid red"
+        }else if(i===show.length){
+            for (var j = 0; j <nodeValue.length; j++){
+                if (nodePoint[j] !== index){
+                    document.getElementById(nodeId[j]).style.left = pointX[nodePoint[j]]+"px"
+                }               
+            }
+            i = nodeValue.length;
+        }
+        i++;
+        if(i > nodeValue.length){       
+            var t = 0;
+            if (index === 0){
+                t=1;
+            }
+            const run2 = setInterval(function(){
+                if (t === 0){
+                    document.getElementById(nodeId[nodeValue.length - 1]).style.left = pointX[nodePoint[nodeValue.length - 1]]+"px"
+                }else if(t === 1){
+                    document.getElementById(nodeId[nodeValue.length - 1]).style.top = "150px"; 
+                }else if (t === 2){
+                    clearInterval(run2);
+                    resetNode()
+                    console.log("stop Insert")
+                }
+                t++;
+            },1000)
+            clearInterval(run);
+        }
+    },1000)
 }
+function deleteNodeShow(index) {
+    ll.removeFrom(index);
+    if (document.getElementById("move-node")){
+        document.getElementById("move-node").remove()
+    }
+
+    var show = []
+    var number = []
+    for (var i = 0; i < nodeValue.length; i++) {
+        if (nodePoint[i] <= index) {
+            show.push(nodeId[i])
+            number.push(nodePoint[i])
+        }
+    }
+    NodeSort(show, number)
+    var i = 0;
+    const run = setInterval(function () {
+        if (i < show.length) {
+            document.getElementById(show[i]).style.border = "2px solid red"
+        } else if(i === show.length){
+            var pointDelete
+            for (var j = 0; j < nodeValue.length; j++) {
+                if (nodePoint[j] === index){
+                    pointDelete = j
+                }
+            }
+            for (var j = 0; j < nodeValue.length; j++) {
+                if (nodePoint[j] > index) {
+                    nodePoint[j] -= 1
+                }
+            }
+            document.getElementById(show[i-1]).style.backgroundColor = "red"
+            document.getElementById(show[i-1]).id = "move-node"
+            nodeValue.splice(pointDelete, 1);
+            nodePoint.splice(pointDelete, 1);
+            nodeId.splice(pointDelete, 1);
+        }
+        if(i > show.length){
+            var t = 0;
+            const run2 = setInterval(function(){
+                if (t === 0){
+                    document.getElementById("move-node").style.top = "280px"
+                }else if(t === 1){
+                    document.getElementById("move-node").style.left = "60px"; 
+                }else if (t === 2){
+                    console.log("stop Delete")
+                    clearInterval(run2);
+                    resetNode()
+                }
+                t++;
+            },1000)
+            clearInterval(run);
+        }
+        i++;
+    }, 1000)
+}
+
+
+
