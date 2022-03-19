a1b422182d363016026d0406360dc27be8a304f9
congphamtienthanh <congphamtienthanh@gmail.com>
2022-03-19 20:42:18 +0700

    api binance

--- Project11_data_binance/binance.js
+++ Project11_data_binance/binance.js
@@ -1,17 +1,28 @@
 var data_binance = [];
-var data
 function getBinance() {
     var current = "https://quanlychitieuthanhcong.000webhostapp.com/jsonPosessing/binance_api/1";
-    var current = "http://quan_ly_chi_tieu.test/jsonPosessing/binance_api/1"
     $.ajax({
-        type: "GET",
-        url: current,
+        type: "",
+        url: "https://quanlychitieuthanhcong.000webhostapp.com/jsonPosessing/binance_api/1",
         data: "",
         dataType: "json",
         success: function (response) {
-            data = response
+            var data = response
             data = data.data
 
+            for (var i = 0; i < data.length; i++) {
+                // data người bán
+                data_binance[i].name = data[i].advertiser.nickName // Tên
+                data_binance[i].orderCount = data[i].advertiser.monthOrderCount // Số lượng đã bán
+                data_binance[i].success = data[i].advertiser.monthFinishRate // tỷ lệ thành công
+                data_binance[i].id = data[i].advertiser.userNo // id người bán
+
+                // data hàng
+                data_binance[i].price = data[i].adv.price // Giá
+                data_binance[i].CanPrice = data[i].adv.tradableQuantity // Số lượng USDT có sẵn
+                data_binance[i].minPrice = data[i].adv.minSingleTransAmount // giới hạn mua nhỏ nhất
+                data_binance[i].maxPrice = data[i].adv.minSingleTransAmount + data[0].adv.maxSingleTransQuantity // gới hạn mua lớn nhất
+            }
 
         }
     });
